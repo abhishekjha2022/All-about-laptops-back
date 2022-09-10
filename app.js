04254c1fbbad00f90025f8407b59fa7f155cb049
Abhishek jha <abhishek@talquo.com>
2022-09-10 20:39:40 +0545

    All codes

---
+++ app.js
@@ -0,0 +1,41 @@
+const express = require("express");
+const mongoose = require("mongoose");
+const cors = require("cors");
+const morgan = require("morgan");
+const bodyParser = require("body-parser");
+const cookieParser = require("cookie-parser");
+const expressValidator = require("express-validator");
+require("dotenv").config();
+const authRoutes = require("./routes/auth");
+const userRoutes = require("./routes/user");
+const categoryRoutes = require("./routes/category");
+const productRoutes = require("./routes/product");
+
+//app
+const app = express();
+
+//db
+
+mongoose.connect(process.env.DATABASE).then(() => {
+  console.log("DB connected");
+});
+
+//middlewares
+
+app.use(morgan("dev"));
+app.use(bodyParser.json());
+app.use(cookieParser());
+app.use(expressValidator());
+app.use(cors());
+
+//routes middleware
+app.use("/api", authRoutes);
+app.use("/api", userRoutes);
+app.use("/api", categoryRoutes);
+app.use("/api", productRoutes);
+
+const port = process.env.PORT || 8000;
+
+app.listen(port, () => {
+  console.log(`Listening to port ${port}`);
+});
